871b6e7f8edc0b95c7a66e4b293b7dc919bf694c
Ivaanmc <ivanmolina112004@gmail.com>
2025-01-14 13:33:28 +0100

    Cambio de volumenes y contraseña de phpmyadmin en docker-composePython

--- web/bd.py
+++ web/bd.py
@@ -1,9 +1,19 @@
 import os
 import pymysql
+from pymysql.err import OperationalError, InternalError
 
 def obtener_conexion():
-    return pymysql.connect(host=os.environ.get('DB_HOST'),
-                                user=os.environ.get('DB_USERNAME'),
-                                password=os.environ.get('DB_PASSWORD'),
-                                port=int(os.environ.get('DB_PORT', 3306)),
-                                db=os.environ.get('DB_DATABASE'))
+    try:
+        conexion = pymysql.connect(
+            host=os.environ.get('DB_HOST'),
+            user=os.environ.get('DB_USERNAME'),
+            password=os.environ.get('DB_PASSWORD'),
+            port=int(os.environ.get('DB_PORT', 3306)),
+            db=os.environ.get('DB_DATABASE')
+        )
+        print("Conexión exitosa a la base de datos")
+        return conexion
+    except (OperationalError, InternalError) as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
+
