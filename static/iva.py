7de29b0a35603bdcdb9188903bcaff2be76f3902
markiclase <markiclase@gmail.com>
2025-01-20 12:31:29 +0100

    Commit de prueba, haciendo cambios a la carpeta static. Añadido apache en el dockercompose.yml

---
+++ static/iva.py
@@ -0,0 +1,8 @@
+def calculariva(importe):
+    """
+    Calcula el IVA (21%) de un importe dado.
+    :param importe: El precio base sin IVA.
+    :return: El IVA calculado.
+    """
+    return importe * 0.21
+
---
+++ static/test-calculos.py
@@ -0,0 +1,10 @@
+import unittest
+from iva import calculariva  
+
+class TestCalcularIva(unittest.TestCase):
+    def test_calcular_iva(self):
+        self.assertEqual(calculariva(100), 21)
+
+if __name__ == "__main__":
+    unittest.main()
+
