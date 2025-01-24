de34329fa9752464e6e7dad1d29b659ce93a0ee6
markiclase <markiclase@gmail.com>
2025-01-24 10:31:47 +0100

    Peliculas añadididas a la cartelera

--- Peliculas.sql
+++ Peliculas.sql
@@ -14,3 +14,6 @@ CREATE TABLE usuarios(
     fechaUltimoAcceso DATE
 );
 INSERT INTO `usuarios` (`usuario`, `clave`, `perfil`, `fechaUltimoAcceso`) VALUES ('root', '1234', 'admin', '2022-03-01');
+INSERT INTO `peliculas` (`id`, `nombre`, `descripcion`, `precio`, `foto`) VALUES ('1', 'Spiderman', 'Nueva pelicula de Spiderman', '5', 'https://img2.rtve.es/i/?w=1600&i=1442912664626.jpg');
+INSERT INTO `peliculas` (`id`, `nombre`, `descripcion`, `precio`, `foto`) VALUES ('2', 'Batman', 'Nueva pelicula de Batman', '5', 'https://statics.forbesargentina.com/2022/06/629f81fe391f3.jpg');
+INSERT INTO `peliculas` (`id`, `nombre`, `descripcion`, `precio`, `foto`) VALUES ('3', 'Superman', 'Nueva pelicula de Superman', '5', 'https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2022/08/03/16595421832009.jpg');
