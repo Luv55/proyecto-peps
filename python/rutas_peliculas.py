5793cb80f58f53a3a56f2abac38583e24104a5b7
markiclase <markiclase@gmail.com>
2025-03-14 11:46:48 +0100

    Cambiado chuches por Peliculas. Cambiado campo ingredientes por reparto.

--- python/rutas_peliculas.py
+++ python/rutas_peliculas.py
@@ -4,22 +4,22 @@ from __main__ import app
 import python.controlador_peliculas as controlador_peliculas
 from funciones_auxiliares import Encoder, sanitize_input, prepare_response_extra_headers,validar_session_admin,validar_session_normal
 
-@app.route("/chuches",methods=["GET"])
-def chuches():
+@app.route("/peliculas",methods=["GET"])
+def peliculas():
     if (validar_session_normal()):
-        respuesta,code= controlador_peliculas.obtener_chuches()
+        respuesta,code= controlador_peliculas.obtener_peliculas()
     else:
         respuesta={"status":"Forbidden"}
         code=403
     response= make_response(json.dumps(respuesta, cls=Encoder), code)
     return response
 
-@app.route("/chuches/<id>",methods=["GET"])
-def chuche_por_id(id):
+@app.route("/peliculas/<id>",methods=["GET"])
+def pelicula_por_id(id):
     id = sanitize_input(id)
     if isinstance(id, str) and len(id)<64:
         if (validar_session_normal()):
-            respuesta,code = controlador_peliculas.obtener_chuche_por_id(id)
+            respuesta,code = controlador_peliculas.obtener_pelicula_por_id(id)
         else:
             respuesta={"status":"Forbidden"}
             code=403
@@ -29,21 +29,21 @@ def chuche_por_id(id):
     response= make_response(json.dumps(respuesta, cls=Encoder), code)
     return response
 
-@app.route("/chuches",methods=["POST"])
-def guardar_chuche():
+@app.route("/peliculas",methods=["POST"])
+def guardar_pelicula():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        chuche_json = request.json
-        if "nombre" in chuche_json and "descripcion" in chuche_json and "foto" in chuche_json and "ingredientes" in chuche_json:
-            nombre = sanitize_input(chuche_json["nombre"])
-            descripcion = sanitize_input(chuche_json["descripcion"])
-            precio = chuche_json["precio"]
-            foto = sanitize_input(chuche_json["foto"])
-            ingredientes = sanitize_input(chuche_json["ingredientes"])
+        pelicula_json = request.json
+        if "nombre" in pelicula_json and "descripcion" in pelicula_json and "foto" in pelicula_json and "ingredientes" in pelicula_json:
+            nombre = sanitize_input(pelicula_json["nombre"])
+            descripcion = sanitize_input(pelicula_json["descripcion"])
+            precio = pelicula_json["precio"]
+            foto = sanitize_input(pelicula_json["foto"])
+            ingredientes = sanitize_input(pelicula_json["ingredientes"])
             if isinstance(nombre, str) and isinstance(descripcion, str) and isinstance(foto, str) and isinstance(ingredientes, str) and len(nombre)<128 and len(descripcion)<512 and len(foto)<128 and len(ingredientes)<512:
                 if (validar_session_admin()):
                     precio = float(precio)
-                    respuesta,code=controlador_peliculas.insertar_chuche(nombre, descripcion,precio,foto,ingredientes)
+                    respuesta,code=controlador_peliculas.insertar_pelicula(nombre, descripcion,precio,foto,ingredientes)
                 else: 
                     respuesta={"status":"Forbidden"}
                     code=403
@@ -59,33 +59,33 @@ def guardar_chuche():
     response= make_response(json.dumps(respuesta, cls=Encoder))  
     return response
 
-@app.route("/chuches/<int:id>", methods=["DELETE"])
-def eliminar_chuche(id):
+@app.route("/peliculas/<int:id>", methods=["DELETE"])
+def eliminar_pelicula(id):
     if (validar_session_admin()):
-        respuesta,code=controlador_peliculas.eliminar_chuche(id)
+        respuesta,code=controlador_peliculas.eliminar_pelicula(id)
     else: 
         respuesta={"status":"Forbidden"}
         code=403
     response= make_response(json.dumps(respuesta, cls=Encoder), code)
     return response
 
-@app.route("/chuches", methods=["PUT"])
-def actualizar_chuche():
+@app.route("/peliculas", methods=["PUT"])
+def actualizar_pelicula():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        chuche_json = request.json
-        if "id" in chuche_json and "nombre" in chuche_json and "descripcion" in chuche_json and "foto" in chuche_json and "ingredientes" in chuche_json:
+        pelicula_json = request.json
+        if "id" in pelicula_json and "nombre" in pelicula_json and "descripcion" in pelicula_json and "foto" in pelicula_json and "ingredientes" in pelicula_json:
             id = request.json["id"]
-            nombre = sanitize_input(chuche_json["nombre"])
-            descripcion = sanitize_input(chuche_json["descripcion"])
-            precio = chuche_json["precio"]
-            foto = sanitize_input(chuche_json["foto"])
-            ingredientes = sanitize_input(chuche_json["ingredientes"])
+            nombre = sanitize_input(pelicula_json["nombre"])
+            descripcion = sanitize_input(pelicula_json["descripcion"])
+            precio = pelicula_json["precio"]
+            foto = sanitize_input(pelicula_json["foto"])
+            ingredientes = sanitize_input(pelicula_json["ingredientes"])
             if id.isnumeric() and isinstance(nombre, str) and isinstance(descripcion, str) and precio.isnumeric() and isinstance(foto, str) and isinstance(ingredientes, str) and len(id)<8 and len(nombre)<128 and len(descripcion)<512 and len(foto)<128 and len(ingredientes)<512:
                 id=int(id)
                 precio=float(precio)
                 if (validar_session_normal()):
-                    respuesta,code=controlador_peliculas.actualizar_chuche(id,nombre,descripcion,precio,foto,ingredientes)
+                    respuesta,code=controlador_peliculas.actualizar_pelicula(id,nombre,descripcion,precio,foto,ingredientes)
                 else: 
                     respuesta={"status":"Forbidden"}
                     code=403
